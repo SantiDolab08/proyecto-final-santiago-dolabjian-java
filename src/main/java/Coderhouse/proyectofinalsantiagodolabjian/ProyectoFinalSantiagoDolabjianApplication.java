b0a70a0aec15f14a70e00261bb694e008e6734a0
Santiago Dolabjian <santi.dolabjian@gmail.com>
2023-06-15 16:10:12 -0300

    Comienzo del proyecto

---
+++ src/main/java/Coderhouse/proyectofinalsantiagodolabjian/ProyectoFinalSantiagoDolabjianApplication.java
@@ -0,0 +1,13 @@
+package Coderhouse.proyectofinalsantiagodolabjian;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoFinalSantiagoDolabjianApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoFinalSantiagoDolabjianApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Coderhouse/proyectofinalsantiagodolabjian/ProyectoFinalSantiagoDolabjianApplicationTests.java
@@ -0,0 +1,9 @@
+package Coderhouse.proyectofinalsantiagodolabjian;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoFinalSantiagoDolabjianApplicationTests {
+
+}
